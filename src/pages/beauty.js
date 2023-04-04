e9f405d904c10be7f8a9ed2f1dcfec606afad0c1
ALEX LEONKOV <al@process-science.com>
2023-04-04 18:46:59 +0200

    changed again

--- src/pages/beauty.js
+++ src/pages/beauty.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import BeautyScreen from "../screens/Beauty/BeautyScreen";
+import { useRouter } from "next/router";
 
 const BeautyPage = ({ clicked, setClicked }) => {
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  });
+
+  
+
   return <BeautyScreen clicked={clicked} setClicked={setClicked} />;
 };
 
